9186e04a5b46d9f40feca4ab49bd373d4a4c52b5
WALTER DA SILVA LUIZ JUNIOR <walter.sljunior@senacsp.edu.br>
2020-03-04 19:44:24 -0300

    aula 3

---
+++ database/migrations/2020_02_27_005153_cria_tabela_produtos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CriaTabelaProdutos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('desc');
+            $table->double('preço');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
